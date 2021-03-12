7fea221aea19d7c1841fe0296680413a7f8d4685
StasB95 <1995bsa@gmail.c0m>
2021-03-12 20:52:32 +0200

    Исправление ошибок

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var popup = document.querySelector('.registration');
 var openPopupButton = document.querySelector('.button-search-hotel');
 
+
 openPopupButton.addEventListener('click', function () {
     console.log('Клик по кнопке');
     popup.classList.add('modal--show');
 });
+
